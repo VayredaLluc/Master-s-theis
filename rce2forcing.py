d5808648846c68de9d321dcdaa4f7814ed25616e
VayredaLluc <73190993+VayredaLluc@users.noreply.github.com>
2025-10-16 17:06:19 +0200

    Add files via upload

--- rce2forcing.py
+++ rce2forcing.py
@@ -23,13 +23,13 @@ def write_forcing_files(lrad_rce,
     #Longwave radiation
     lrad = np.tile(lrad_day,year_len)
     lrad = lrad/np.mean(lrad)*lrad_rce
-    np.savetxt('lrad.txt', lrad)
+    np.savetxt('lrad', lrad)
 
     #Shortwave radiation
     srad_day = np.maximum(-np.cos(day_time*2*np.pi/day_len),0)
     srad = np.tile(srad_day,year_len)
     srad = srad/np.mean(srad)*srad_rce
-    np.savetxt('srad.txt', srad)
+    np.savetxt('srad', srad)
 
     #Rain
     rain_day = 1 - np.sin(day_time*2*np.pi/day_len+4*np.pi/day_len)
@@ -38,27 +38,27 @@ def write_forcing_files(lrad_rce,
     for r in daily_precip:
         rain = np.append(rain,rain_day*r)
     rain = rain/np.sum(rain)*year_len*rain_rce/1000
-    np.savetxt('rain.txt', rain)
+    np.savetxt('rain', rain)
 
     #Relative Humidity
     #rhum_day = 1-(1-rhum_rce)*2*np.sin(day_time*np.pi/24-np.pi/4)**2
     rhum_day = rhum_rce+(1-rhum_rce)/2*np.sin(day_time*2*np.pi/day_len)
     rhum = np.tile(rhum_day,year_len)
-    np.savetxt('rhum.txt', rhum)
+    np.savetxt('rhum', rhum)
 
     #Air temperature
     tair_day = -3*np.sin(day_time*2*np.pi/day_len)
     tair = np.tile(tair_day,year_len)
     tair = tair-np.mean(tair)+tair_rce
-    np.savetxt('tair.txt', tair)
+    np.savetxt('tair', tair)
 
     #Wind speed
     wind = np.tile(wind_day*wind_rce,year_len)
-    np.savetxt('wind.txt', wind)
+    np.savetxt('wind', wind)
 
     #Snow
     snow = np.tile(snow_day,year_len)
-    np.savetxt('snow.txt', snow)
+    np.savetxt('snow', snow)
 
     return
 
